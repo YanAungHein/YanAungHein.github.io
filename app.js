99a05673a9575ab7a70f25592d63ae35d0004bba
YanAungHein <59559794+YanAungHein@users.noreply.github.com>
2020-02-09 19:49:43 +0630

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const burger = document.querySelector('.bars');
+const menu = document.querySelector('.menu-toggle');
 const navList = document.querySelector('.nav-list');
 const navItem = document.querySelectorAll('.nav-item');
-const bars = document.querySelector('.bars');
 
-burger.addEventListener("click", () => {
+menu.addEventListener("click", () => {
     navList.classList.toggle('nav-open');
 
-    bars.classList.toggle('bar-open');
+    menu.classList.toggle('bar-open');
 
     navItem.forEach(item => {
         item.classList.toggle('item-open');
